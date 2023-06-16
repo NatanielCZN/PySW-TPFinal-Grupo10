723460ec917228b35644907e4e4f48bfc7ce5d7c
rochadgps <81383227+rochadgps@users.noreply.github.com>
2023-06-15 22:26:37 -0300

    subiendo 1 servicio

---
+++ backend/models/servicio.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ServicioSchema = new Schema({
+    nombre: { type: String, required: true },
+    categoria: { type: String, required: true },
+    ubicacion: { type: String, required: true },
+    calificacionTotal: { type: Number, required: true },
+    //resenia: {type: Schema.Types.ObjectId, ref: Resenia, required: true },
+    usernameGestor: { type: String, required: true }
+})
+
+module.exports = mongoose.models.Servicio || mongoose.model('Servicio', ServicioSchema);
