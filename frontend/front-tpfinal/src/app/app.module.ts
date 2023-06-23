c80639572994fa62515bce7e7b2e445be7c14b1e
userJoA <jo1gallardo@gmail.com>
2023-06-23 10:30:37 -0300

    service de Reserva
    .

--- frontend/front-tpfinal/src/app/app.module.ts
+++ frontend/front-tpfinal/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginService } from './services/login.service';
-import {HttpClientModule} from '@angular/common/http';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { GestorComponent } from './components/gestor/gestor.component';
 import { AdminComponent } from './components/admin/admin.component';
