290f6180336b039cd2a42cc51547b3b70ac052db
Nataniel Cazon <natanielarg@gmail.com>
2023-06-24 22:00:41 -0300

    Fix Ciudades

--- frontend/front-tpfinal/src/app/app-routing.module.ts
+++ frontend/front-tpfinal/src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { GestorComponent } from './components/gestor/gestor.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { CiudadesComponent } from './components/ciudades/ciudades.component';
 
 const routes: Routes = [
-  {path:"login",component:LoginComponent},
-  {path:"usuario",component:UsuarioComponent},
-  {path:"gestor",component:GestorComponent},
-  {path:"admin",component:AdminComponent},
-  {path: 'home', component:HomeComponent},
-  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+  { path: "login", component: LoginComponent },
+  { path: "usuario", component: UsuarioComponent },
+  { path: "gestor", component: GestorComponent },
+  { path: "admin", component: AdminComponent },
+  { path: 'home', component: HomeComponent },
+  //{ path: '**', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'ciudad', component: CiudadesComponent }
 ];
 
 @NgModule({
