fde6c310b212c055e740d2c5014cef785be795f2
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2023-06-27 15:53:14 -0300

    Agregado de API imagenes y maps

--- frontend/front-tpfinal/src/app/components/ciudades/ciudades.component.ts
+++ frontend/front-tpfinal/src/app/components/ciudades/ciudades.component.ts
@@ -10,23 +10,30 @@ import { Provincia } from 'src/app/models/provincia';
 })
 export class CiudadesComponent implements OnInit {
 
-  seleccion!:string;
-  provincia!:Provincia;
-  localidad!:Provincia;
+  seleccion!: string;
+  provincia!: Provincia;  //OBJ tipo Provincia que contiene informacion de la misma
+  localidad!: Provincia;  //OBJ tipo Provincia que contiene solo el nombre y id de las localidades de una Provincia
+  mostrarCard = false;
   localidades!: Array<Provincia>;
-  
+  imagenes: string[] = [];
+
+
   provincias = [{ id: 1, name: "Jujuy" },
   { id: 2, name: "Salta" }, { id: 3, name: "Tucuman" },
-  { id: 4, name: "Sgo. del Estero" }, { id: 5, name: "Buenos Aires" },
+  { id: 4, name: "Santiago del Estero" }, { id: 5, name: "Buenos Aires" },
   { id: 6, name: "Rio Negro" }, { id: 7, name: "Mendoza" },
- ];
+  { id: 8, name: "Chubut" }, { id: 9, name: "Misiones" },
+  ];
 
 
-  constructor(private _http: HttpClient,private ciudadService: CiudadesService) {
+  constructor(private _http: HttpClient, private ciudadService: CiudadesService) {
     this.provincia = new Provincia();
     this.localidades = new Array<Provincia>();
-   }
+  }
 
+  mostrarTarjeta() {
+    this.mostrarCard = true;
+  }
 
   consultarProvincia(nombre: string) {
     this.ciudadService.getProvincias(nombre).subscribe(
@@ -37,6 +44,9 @@ export class CiudadesComponent implements OnInit {
         this.provincia.lat = result.provincias[0].centroide.lat;
         this.provincia.long = result.provincias[0].centroide.lon;
         this.cargarLocalidades(this.provincia._id);
+        this.buscarImagen(this.provincia);
+        this.cargarClima(this.provincia);
+        this.mostrarTarjeta();
       },
       error => { alert("Error en la petición"); })
   }
@@ -44,16 +54,52 @@ export class CiudadesComponent implements OnInit {
   cargarLocalidades(id: string) {
     this.ciudadService.getLocalidades(id).subscribe(
       (result) => {
-        for (let i=0; i != -1; i++){
-        this.localidad = new Provincia();
-        this.localidad.nombre = result.municipios[i].nombre;
-        this.localidad._id = result.municipios[i].id;
+        if (result && result.municipios) {
+          for (let i = 0; i < result.municipios.length; i++) {
+            this.localidad = new Provincia();
+            this.localidad.nombre = result.municipios[i].nombre;
+            this.localidad._id = result.municipios[i].id;
+            this.localidades.push(this.localidad);
+          }
+        } else {
+          console.error('La respuesta del servicio no tiene la estructura esperada.');
+        }
+      },
+      error => { alert("Error en la petición"); }
+    );
+  }
+  
 
-        this.localidades.push(this.localidad);
-    }
+  buscarImagen(provincia: Provincia) {
+    this.ciudadService.buscarImagenPorPalabraClave(provincia.nombre).subscribe(
+      (response) => {
+        // Aquí puedes manejar la respuesta de la API de Unsplash
+        var i;
+        console.log(response);
+        this.imagenes= [];
+        for (const resultado of response.results) {
+          const imagen = resultado.urls.small;
+          this.imagenes.push(imagen);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  cargarClima(provincia:Provincia) {
+    this.ciudadService.getClima(provincia.lat,provincia.long).subscribe(
+      (result) => {
+        console.log(result);
+        this.provincia.clima = "temp max : " +result.ClimateDataMonth[7].tmax+ ", temp min : "+result.ClimateDataMonth[7].tmin;
       },
       error => { alert("Error en la petición"); })
   }
+
+
+  
+
   ngOnInit(): void {
   }
 
