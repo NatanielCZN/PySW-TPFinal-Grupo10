1c29163b1ff6007170dac05496c4f319a7a4eb4e
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2023-06-30 18:06:44 -0300

    component admin

--- frontend/front-tpfinal/src/app/components/roles/admin/admin.component.ts
+++ frontend/front-tpfinal/src/app/components/roles/admin/admin.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
 import { AppComponent } from 'src/app/app.component';
+import { Gestor } from 'src/app/models/gestor';
+import { Usuario } from 'src/app/models/usuario.model';
+import { GestorService } from 'src/app/services/gestor.service';
+import { ServiciosService } from 'src/app/services/servicios.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-admin',
@@ -10,8 +16,14 @@ export class AdminComponent implements OnInit {
   
   id:any;
   tipo:any
-  constructor(private appCom:AppComponent) { 
+  gestores!:Array<Gestor>;
+  usuarios!:Array<Usuario>;
+
+
+  constructor(private appCom:AppComponent, private gestorServicio:GestorService, private usuarioService:UsuarioService, private servicioService:ServiciosService) { 
    this.appCom.logeado=true;
+   this.cargarGestores();
+   this.cargarUsuarios();
   }
 
   ngOnInit(): void {
@@ -19,4 +31,37 @@ export class AdminComponent implements OnInit {
     this.tipo=sessionStorage.getItem("tipo");
   }
 
+  cargarServiciosGestor(){
+    
+  }
+
+   cargarGestores() {
+    this.gestorServicio.getGestores().subscribe(
+      (result) => {
+          console.log(result)
+          this.gestores = new Array<Gestor>();
+          for (let i = 0; i < result.length; i++) {
+            let unGestor = new Gestor();
+            unGestor = result[i];
+            this.gestores.push(unGestor);
+          }
+          },
+      error => { alert("Error en la petición"); }
+    );
+  }
+  
+  cargarUsuarios() {
+    this.usuarioService.getusuarios().subscribe(
+      (result) => {
+          console.log(result)
+          this.usuarios = new Array<Usuario>();
+          for (let i = 0; i < result.length; i++) {
+            let unUsuario = new Usuario();
+            unUsuario = result[i];
+            this.usuarios.push(unUsuario);
+          }
+          },
+      error => { alert("Error en la petición"); }
+    );
+  }
 }
