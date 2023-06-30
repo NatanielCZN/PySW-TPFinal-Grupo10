ea52fc3cc24a4266725cceb68bf9fe7b76ffb282
Nataniel Cazon <natanielarg@gmail.com>
2023-06-29 22:23:26 -0300

    Fix relasiones front

--- frontend/front-tpfinal/src/app/models/usuario.model.ts
+++ frontend/front-tpfinal/src/app/models/usuario.model.ts
@@ -1,23 +1,28 @@
 import { Login } from "./login.model";
+import { Resenia } from "./resenia";
+import { Reserva } from "./reserva";
 
-export class Usuario extends Login{
-    _id:string;
-    nombre:string;
-    apellido:string
-    email:string;
-    dni:string;
-    fechaNacimiento:string;
-    edad:number;
+export class Usuario extends Login {
+    _id: string;
+    nombre: string;
+    apellido: string
+    email: string;
+    dni: string;
+    fechaNacimiento: string;
+    edad: number;
+    reserva!: Array<Reserva>;
+    resenia!: Array<Resenia>;
 
-    constructor(){
+    constructor() {
         super();
-       this._id='';
-       this.nombre='';
-       this.apellido='';
-       this.email='';
-       this.dni='';
-       this.fechaNacimiento='';
-       this.edad=0;
-
+        this._id = '';
+        this.nombre = '';
+        this.apellido = '';
+        this.email = '';
+        this.dni = '';
+        this.fechaNacimiento = '';
+        this.edad = 0;
+        this.reserva = new Array<Reserva>();
+        this.resenia = new Array<Resenia>();
     }
 }
