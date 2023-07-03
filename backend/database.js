b9b662a8d0a67be9dfa6c315b28df47a5ec37c05
Nataniel Cazon <natanielarg@gmail.com>
2023-07-02 21:00:23 -0300

    BDD habilitada

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://0.0.0.0/proyecto-final';
-//const URI = 'mongodb+srv://rochadgps:DeTksWUEYf5V18Rt@cluster0.od6xiua.mongodb.net/';
+//const URI = 'mongodb://0.0.0.0/proyecto-final';
+const URI = 'mongodb+srv://rochadgps:DeTksWUEYf5V18Rt@cluster0.od6xiua.mongodb.net/';
 //const URI = 'mongodb://localhost/proyecto-final';
 
 mongoose.connect(URI)
