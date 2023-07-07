84483ed59210b46a239f22416c2aa8820cd3f48e
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2023-07-07 16:49:55 -0300

    perfil admin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,10 +5,16 @@ const authCtl = {}
 
 authCtl.verifyToken = async (req, res, next) => {
     //Para los headers de autenticasion 
+
+    if (req.user === 'admin') {
+        // Si el usuario es de tipo admin, se permite el acceso a todos los routes
+        next();}
+
     if (!req.headers || !req.headers.authorization) {
         res.json({ 'status': '0', 'msg': 'Unauthorized request' })
         return;
     }
+
     var arrayTexto = req.headers.authorization.split(' ');
     var token = null;
     (arrayTexto.length >= 2) ? token = arrayTexto[1] : token = null;
@@ -16,10 +22,14 @@ authCtl.verifyToken = async (req, res, next) => {
         res.json({ 'status': '0', 'msg': 'Unauthorized request.' });
     } else {
         try {
+            
             const payload = jwt.verify(token, "secretkey");
             //payload retorna la información del user que se uso en el método de login
             req.userId = payload._id;
+            
             next(); //se pasa a procesar el siguiente método del stack de la peticion
+
+            
         } catch (error) {
             res.json({ 'status': '0', 'msg': 'Unauthorized request.' });
         }
