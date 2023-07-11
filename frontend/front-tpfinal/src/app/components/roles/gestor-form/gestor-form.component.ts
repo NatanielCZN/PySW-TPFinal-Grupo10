33c5d4a784f86bf2c8a82e084498c7e6bd5b25a0
Nataniel Cazon <natanielarg@gmail.com>
2023-07-10 23:37:57 -0300

    Change gestor form and add gestor datos component

--- frontend/front-tpfinal/src/app/components/roles/gestor-form/gestor-form.component.ts
+++ frontend/front-tpfinal/src/app/components/roles/gestor-form/gestor-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gestor } from 'src/app/models/gestor';
 import { GestorService } from 'src/app/services/gestor.service';
 
@@ -13,7 +13,7 @@ export class GestorFormComponent implements OnInit {
 
   gestor!: Gestor;
 
-  constructor(private gestorService: GestorService, private router: Router) {
+  constructor(private gestorService: GestorService, private router: Router, private route: ActivatedRoute) {
     this.gestor = new Gestor();
   }
 
@@ -31,10 +31,16 @@ export class GestorFormComponent implements OnInit {
       .subscribe(
         (res: any) => {
           console.log(res);
+
+          this.router.navigate(['/login']);
         },
         err => {
           console.log(err);
         }
       )
+  }
+
+  volverAlInicio(): void {
+    this.router.navigate(['./home']);
   }
 }
